d7a329097249c112f8fbbda303ff4edb31b56257
wabudd1 <1421354+wabudd1@users.noreply.github.com>
2025-05-08 19:39:31 -0400

    blech

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { DashComponent } from "./dash/dash.component";
 
 @Component({
   selector: 'ham-root',
   standalone: true,
   templateUrl: './app.component.html',
   styles: [],
+  imports: [DashComponent],
 })
 export class AppComponent {
   title = 'hamDash';
