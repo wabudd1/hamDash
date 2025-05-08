2f65d0da2ee7a78d7431b114c63c175ff473e5fc
wabudd1 <1421354+wabudd1@users.noreply.github.com>
2025-05-08 19:23:57 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'ham-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
---
+++ src/app/dash/dash.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ham-dash',
+  standalone: true,
+  imports: [],
+  templateUrl: './dash.component.html',
+  styleUrl: './dash.component.scss'
+})
+export class DashComponent {
+
+}
